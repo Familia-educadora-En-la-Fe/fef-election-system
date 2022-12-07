0cce7d570df2a02b87b6adc28bfa3f5bfc55c2ac
josecarlos <josecarlospas1@gmail.com>
2022-12-07 14:06:06 -0600

    feat(firebase): add firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import {config} from "./config/config";
 import Home from "./components/Home";
 import './index.css'
+
+const app = initializeApp(config.firebaseConfig);
+export const db = getFirestore(app);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/fef-election-system/" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
